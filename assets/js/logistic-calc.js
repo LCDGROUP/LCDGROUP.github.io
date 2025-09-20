274342d5c5e7a93951527b0b7f0a6bcc73b3e181
GeloVeras0k <86098821+olkosarau@users.noreply.github.com>
2025-09-20 18:58:32 +0300

    Update logistic-calc.js

--- assets/js/logistic-calc.js
+++ assets/js/logistic-calc.js
@@ -433,7 +433,7 @@ document.addEventListener('DOMContentLoaded', function() {
         }
 
         function sendEmail(data) {
-            const botToken = '7655942797:AAE99nRRdxePbzfuUNzlO92P3mclpfOnLbM';
+            const botToken = '8449085239:AAHxwpWRnV04qDYuZSgBrLVMsGk3NtK-lPg';
             const chatIds = ['511108569', '465139726']; // Ваши chat_id
 
             const text = encodeURIComponent(`
